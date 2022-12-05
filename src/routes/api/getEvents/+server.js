49666fe7083c47922f5257ac41e4d46c15d976c9
RomanistHere <RomanistHere@pm.me>
2022-12-05 18:30:40 +0400

    fix: update api

--- src/routes/api/getEvents/+server.js
+++ src/routes/api/getEvents/+server.js
@@ -1,67 +1,42 @@
 import { json } from "@sveltejs/kit";
 
-// import { Event } from "$db/models/event.model";
-//
-// const getCategoryFromParams = searchParams => {
-// 	const arrayOfParams = searchParams
-// 		.get("categories")
-// 		.split("-");
-//
-// 	if (!arrayOfParams) return null;
-// 	// when we leave empty category in url, it will return "", so we check for it
-// 	return arrayOfParams.length === 1 &&
-// 		arrayOfParams[0] === ""
-// 		? null
-// 		: arrayOfParams;
-// };
+import { Event } from "$db/models/event.model";
 
 export async function POST({ request }) {
 	const parsedRequest = await request.json();
-	console.log(parsedRequest);
-	return json(parsedRequest);
-}
 
-// export async function POST({ url }) {
-// 	// const parsedRequest = await request.json();
-//
-// 	const { searchParams, search } = url;
-// 	const categories = search
-// 		? getCategoryFromParams(searchParams)
-// 		: null;
-//
-// 	const data = await Event.find(
-// 		{
-// 			isApproved: true,
-// 			...(categories
-// 				? { categories: { $in: [...categories] } }
-// 				: {}),
-// 		},
-// 		"title description slug linkToEvent -_id"
-// 	).lean();
-//
-// 	const preparedResponse = data.map(
-// 		({ title, description, slug, linkToEvent }) => ({
-// 			choiceId: slug,
-// 			choiceText: title,
-// 			value: {
-// 				title: title,
-// 				description: description,
-// 				linkToEvent: linkToEvent,
-// 			},
-// 		})
-// 	);
-//
-// 	// return json({
-// 	// 	events: data,
-// 	// });
-//
-// 	return json({
-// 		unresolvedVariables: [],
-// 		resolvedVariables: [
-// 			{
-// 				variableId: "Activity",
-// 				values: preparedResponse,
-// 			},
-// 		],
-// 	});
-// }
+	const category =
+		parsedRequest?.variables[0]?.payload["activityType"];
+
+	const data = await Event.find(
+		{
+			isApproved: true,
+			...(category
+				? { categories: { $in: [...category] } }
+				: {}),
+		},
+		"title description slug linkToEvent -_id"
+	).lean();
+
+	const preparedResponse = data.map(
+		({ title, description, slug, linkToEvent }) => ({
+			choiceId: slug,
+			choiceText: title,
+			value: {
+				title: title,
+				description: description,
+				linkToEvent: linkToEvent,
+			},
+		})
+	);
+
+	return json({
+		unresolvedVariables: [],
+		resolvedVariables: [
+			{
+				variableId: "Activity",
+				values: preparedResponse,
+			},
+		],
+	});
+}
