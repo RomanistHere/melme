0d353882f3cac8fc5b6eb0ef00ff4fba867c520a
RomanistHere <RomanistHere@pm.me>
2022-12-05 11:38:17 +0400

    fix: add categories

--- src/routes/api/getEvents/+server.js
+++ src/routes/api/getEvents/+server.js
@@ -2,10 +2,31 @@ import { json } from "@sveltejs/kit";
 
 import { Event } from "$db/models/event.model";
 
-export async function POST() {
+const getCategoryFromParams = searchParams => {
+	const arrayOfParams = searchParams
+		.get("categories")
+		.split("-");
+
+	if (!arrayOfParams) return null;
+	// when we leave empty category in url, it will return "", so we check for it
+	return arrayOfParams.length === 1 &&
+		arrayOfParams[0] === ""
+		? null
+		: arrayOfParams;
+};
+
+export async function POST({ url }) {
+	const { searchParams, search } = url;
+	const categories = search
+		? getCategoryFromParams(searchParams)
+		: null;
+
 	const data = await Event.find(
 		{
 			isApproved: true,
+			...(categories
+				? { categories: { $in: [...categories] } }
+				: {}),
 		},
 		"title description linkToEvent -_id"
 	).lean();
