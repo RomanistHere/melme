9ea16c0cdce3a2368acafdc45fa55c3609c34c97
RomanistHere <RomanistHere@pm.me>
2022-12-24 18:44:16 +0400

    fix: format

--- src/lib/components/Map/Map.svelte
+++ src/lib/components/Map/Map.svelte
@@ -26,57 +26,56 @@
 	const dispatch = createEventDispatcher();
 
 	const preparePOIsJson = markerData => ({
-		"type": "FeatureCollection",
-		"features": markerData.map(({ location, slug }) => ({
-			"type": "Feature",
-			"geometry": {
-				"type": "Point",
-				"coordinates": location,
+		type: "FeatureCollection",
+		features: markerData.map(({ location, slug }) => ({
+			type: "Feature",
+			geometry: {
+				type: "Point",
+				coordinates: location,
 			},
-			"properties": {
+			properties: {
 				"image-name": "event-marker",
-				"slug": slug,
+				slug,
 			},
 		})),
 	});
 
 	const displayPins = (map, data) => {
-		if (!map || !data)
-			return;
+		if (!map || !data) return;
 
 		map.loadImage(imagesToLoad["event-marker"], (error, image) => {
 			if (error) throw error;
 
 			map.addImage("event-marker", image, {
-				"sdf": "true",
-				"pixelRatio": 1.5,
+				sdf: "true",
+				pixelRatio: 1.5,
 			});
 
 			const eventsJson = preparePOIsJson(data);
 
 			map.addSource("events", {
-				"type": "geojson",
-				"data": eventsJson,
-				"generateId": true,
+				type: "geojson",
+				data: eventsJson,
+				generateId: true,
 			});
 
 			map.addLayer({
-				"id": "events",
-				"type": "symbol",
-				"source": "events",
-				"layout": {
+				id: "events",
+				type: "symbol",
+				source: "events",
+				layout: {
 					"icon-image": "event-marker",
 					"icon-allow-overlap": true,
 				},
-				"paint": {
+				paint: {
 					// "icon-color": "#9D9D9D", //3f3ec2
 					"icon-color": [
 						"case",
 						["boolean", ["feature-state", "hover"], false],
 						"#3f3ec2",
 						"#9D9D9D",
-					]
-				}
+					],
+				},
 			});
 
 			map.on("movestart", resetPinHoverState);
@@ -144,11 +143,12 @@
 	$: displayPins(map, poisData);
 
 	const createMap = container => {
-		const token = "pk.eyJ1Ijoicm9tYW5pc3RoZXJlIiwiYSI6ImNrc3E2cjYyMTA5eXkyeG5xZXpkcTI0dnUifQ.Bm8W-u4ylJZTzs3sNFu91w";
+		const token =
+			"pk.eyJ1Ijoicm9tYW5pc3RoZXJlIiwiYSI6ImNrc3E2cjYyMTA5eXkyeG5xZXpkcTI0dnUifQ.Bm8W-u4ylJZTzs3sNFu91w";
 		const mapObj = new mapboxgl.Map({
 			accessToken: token,
 			container,
-			center: [ 55.27, 25.23 ],
+			center: [55.27, 25.23],
 			style: "mapbox://styles/romanisthere/clc1u0ef2001d14lms41wjsh8", // style URL
 			minZoom: 4,
 			maxZoom: 18,
@@ -158,8 +158,7 @@
 		});
 		mapObj.addControl(new mapboxgl.NavigationControl(), "bottom-right");
 		// mapObj.on("contextmenu", onMapClick);
-		if (onMapClick)
-			mapObj.on("click", onMapClick);
+		if (onMapClick) mapObj.on("click", onMapClick);
 
 		return mapObj;
 	};
@@ -174,4 +173,7 @@
 	};
 </script>
 
-<div use:mapAction class="w-full h-full"></div>
+<div
+	use:mapAction
+	class="w-full h-full"
+/>
--- src/routes/map/+page.svelte
+++ src/routes/map/+page.svelte
@@ -2,28 +2,30 @@
 	import Seo from "$lib/components/Seo.svelte";
 	import Map from "$lib/components/Map/Map.svelte";
 
-	const poisData = [{
-		location: [ 55.27, 25.23 ],
-		slug: "something",
-		title: "something",
-	}, {
-		location: [ 55.28, 25.18 ],
-		slug: "something",
-		title: "something",
-	}, {
-		location: [ 55.28, 25.184 ],
-		slug: "something",
-		title: "something",
-	}];
+	const poisData = [
+		{
+			location: [55.27, 25.23],
+			slug: "something",
+			title: "something",
+		},
+		{
+			location: [55.28, 25.18],
+			slug: "something",
+			title: "something",
+		},
+		{
+			location: [55.28, 25.184],
+			slug: "something",
+			title: "something",
+		},
+	];
 
 	const handlePoiClick = ({ detail }) => {
 		console.log(detail);
 	};
 </script>
 
-<Seo
-	title="melme | map view"
-/>
+<Seo title="melme | map view" />
 
 <section class="fixed inset-0 w-screen h-screen">
 	<Map
