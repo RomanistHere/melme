e1934bb9a93e5e4f941f205522f49c6262f683cb
RomanistHere <RomanistHere@pm.me>
2023-01-10 00:31:04 +0400

    fix: format

--- src/routes/submit/+page.svelte
+++ src/routes/submit/+page.svelte
@@ -18,9 +18,14 @@
 </div>
 
 <p class="mb-2 text-lg">
-	Event - something that happens in or for a limited period of time, <i>e.g. Burj Khalifa Laser Show</i>.
+	Event - something that happens in or for a limited period of time, <i>
+		e.g. Burj Khalifa Laser Show
+	</i>
+	.
 </p>
 
 <p class="text-lg">
-	Attraction - place of interest that exists and hosts visitors on a daily basis, <i>e.g. Coffee Museum</i>.
+	Attraction - place of interest that exists and hosts visitors on a daily
+	basis, <i>e.g. Coffee Museum</i>
+	.
 </p>
